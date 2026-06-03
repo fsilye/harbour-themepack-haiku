08c7af51ea873b12861738024c3833da6517f49e
Frank Paul Silye <frankps@gmail.com>
2026-06-03 23:14:41 +0200

    Update harbour-themepack-haiku-no_NB.ts
    Lagt inn oversettelsene.

--- translations/harbour-themepack-haiku-no_NB.ts
+++ translations/harbour-themepack-haiku-no_NB.ts
@@ -1,1 +1,80 @@
 
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Thank you for installing %1!</source>
+        <translation>Takk for at du installerte %1!</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>Kildekode</translation>
+    </message>
+    <message>
+        <source>Icon request</source>
+        <translation>Ikonforespørsel</translation>
+    </message>
+    <message>
+        <source>From here you can request missing icons for your favorite apps.</source>
+        <translation>Herfra kan du be om manglende ikoner til favorittappene dine.</translation>
+    </message>
+    <message>
+        <source>This will open your e-mail client, from which you can send the maintainer the name of the apps you would like to be included in this theme.</source>
+        <translation>Dette vil åpne e-postprogrammet ditt, slik at du kan sende utvikleren navnet på appene du ønsker skal inkluderes i dette temaet.</translation>
+    </message>
+    <message>
+        <source>By requesting new icons, you accept sending the name of the unthemed apps installed on your device, along with your e-mail address. This data will be used only for the intended purpose and NEVER disclosed to third parties. Your app names and e-mail address will be deleted right after.</source>
+        <translation>Ved å be om nye ikoner godtar du å sende navnet på appene uten tema installert på enheten din, sammen med e-postadressen din. Disse dataene vil kun bli brukt til det tiltenkte formålet og ALDRI bli utlevert til tredjeparter. Appnavnene og e-postadressen din vil slettes umiddelbart etterpå.</translation>
+    </message>
+    <message>
+        <source>If you plan to request icons, please consider donating! It helps support development and maintenance of the project.</source>
+        <translation>Hvis du planlegger å be om ikoner, vurder gjerne å donere! Det bidrar til å støtte utviklingen og vedlikeholdet av prosjektet.</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Doner</translation>
+    </message>
+    <message>
+        <source>I don&apos;t care donating</source>
+        <translation>Jeg ønsker ikke å donere</translation>
+    </message>
+    <message>
+        <source>Request icons</source>
+        <translation>Be om ikoner</translation>
+    </message>
+    <message>
+        <source>Developers</source>
+        <translation>Utviklere</translation>
+    </message>
+    <message>
+        <source>If you want to create a theme compatible with UI Themer, please read the documentation.</source>
+        <translation>Hvis du ønsker å opprette et tema som er kompatibelt med UI Themer, vennligst les dokumentasjonen.</translation>
+    </message>
+    <message>
+        <source>Documentation</source>
+        <translation>Dokumentasjon</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Oversettelser</translation>
+    </message>
+    <message>
+        <source>Request a new language or contribute to existing languages on the Transifex project page.</source>
+        <translation>Be om et nytt språk eller bidra til eksisterende språk på Transifex-prosjektsiden.</translation>
+    </message>
+    <message>
+        <source>Transifex</source>
+        <translation>Transifex</translation>
+    </message>
+    <message>
+        <source>Disclaimer</source>
+        <translation>Ansvarsfraskrivelse</translation>
+    </message>
+    <message>
+        <source>This theme pack is inspired by Haiku® aesthetics; it is an independent project by UI Themer. This project is not affiliated with or endorsed by %1. Haiku® and HAIKU Leaf™ are trademarks of Haiku, Inc. See %2.</source>
+        <translation>Denne temapakken er inspirert av Haiku®-estetikk; det er et uavhengig prosjekt av UI Themer. Dette prosjektet er ikke tilknyttet eller godkjent av %1. Haiku® og HAIKU Leaf™ er varemerker som tilhører Haiku, Inc. Se %2.</translation>
+    </message>
+</context>
+</TS>
